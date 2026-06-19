9523329c0cfc3e2e9d000f0086f1a432b3ea6701
Aadrizeta <adrianzambranomontero@gmail.com>
2026-06-19 22:30:55 +0200

    Speed Insights istalado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,20 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Footer from "@/components/layout/Footer/Footer";
 import Header from "@/components/layout/Header/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const now = localFont({
   src: [
-    { path: "../../public/fonts/Now-Thin.woff2",       weight: "100", style: "normal" },
+    { path: "../../public/fonts/Now-Thin.woff2", weight: "100", style: "normal" },
     { path: "../../public/fonts/Now-ExtraLight.woff2", weight: "200", style: "normal" },
-    { path: "../../public/fonts/Now-Light.woff2",      weight: "300", style: "normal" },
-    { path: "../../public/fonts/Now-Regular.woff2",    weight: "400", style: "normal" },
-    { path: "../../public/fonts/Now-Medium.woff2",     weight: "500", style: "normal" },
-    { path: "../../public/fonts/Now-SemiBold.woff2",   weight: "600", style: "normal" },
-    { path: "../../public/fonts/Now-Bold.woff2",       weight: "700", style: "normal" },
-    { path: "../../public/fonts/Now-ExtraBold.woff2",  weight: "800", style: "normal" },
-    { path: "../../public/fonts/Now-Black.woff2",      weight: "900", style: "normal" },
+    { path: "../../public/fonts/Now-Light.woff2", weight: "300", style: "normal" },
+    { path: "../../public/fonts/Now-Regular.woff2", weight: "400", style: "normal" },
+    { path: "../../public/fonts/Now-Medium.woff2", weight: "500", style: "normal" },
+    { path: "../../public/fonts/Now-SemiBold.woff2", weight: "600", style: "normal" },
+    { path: "../../public/fonts/Now-Bold.woff2", weight: "700", style: "normal" },
+    { path: "../../public/fonts/Now-ExtraBold.woff2", weight: "800", style: "normal" },
+    { path: "../../public/fonts/Now-Black.woff2", weight: "900", style: "normal" },
   ],
   variable: "--font-now",
   display: "swap",
